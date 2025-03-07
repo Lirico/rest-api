2bf13053301b7ec08a6d47740417ff20b53efcb6
lirico <dominguezmatiasadrian@gmail.com>
2025-03-07 15:33:36 -0300

    model config

--- readme.md
+++ readme.md
@@ -1,8 +1,22 @@
 
-
-
 ## Dependencias
+express
+http  
+bodyParser  
+cookieParser  
+compression  
+cors 
+mongoose
 
+@types/express
+@types/http  
+@types/bodyParser  
+@types/cookieParser  
+@types/compression  
+@types/cors 
+@types/mongoose
+
+## Dev Dependencias
 typescript
 ts-node
 nodemon
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from 'cors';
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -19,3 +20,9 @@ const server = http.createServer(app);
 server.listen(8080, () => {
     console.log('Server running on http://localhost:8080/');
 })
+
+const MONGO_URL = 'mongodb+srv://numen:numen123@clasenumen.og6nr3z.mongodb.net/'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error',(error: Error) => console.log(error))
